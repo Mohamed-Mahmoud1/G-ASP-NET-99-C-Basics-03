f229392b743e3d213a29aefdd81227424625766e
Mohamed-Mahmoud1 <mohamedzanaty.dev@gmail.com>
2026-08-30 20:59:42 +0300

    Question03

--- project03/Program.cs
+++ project03/Program.cs
@@ -15,21 +15,30 @@
 
             #region Question02
             //2. Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
-            string title = "Refactoring";
-            switch (title)
-            {
-                case "Clean Code":
-                    Console.WriteLine("Great choice!");
-                    break;
-                case "Refactoring":
-                    Console.WriteLine("Nice pick!");
-                    break;
-                default:
-                    Console.WriteLine("Never heard of it"); 
-                    break;
-            }
+            //string title = "Refactoring";
+            //switch (title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Never heard of it"); 
+            //        break;
+            //}
 
             #endregion
+
+            #region Question03
+            //3. Given int pages = 464;, use the ternary operator to store "Long Book" or "Short Book" into a variable sizeLabel (same rule as question 6: long if pages > 300).
+            //int pages = 464;
+            //string sizeLabel = pages>300?"Long Book" : "Short Book";
+            //Console.WriteLine(sizeLabel);
+            #endregion
         }
+
+
     }
 }
